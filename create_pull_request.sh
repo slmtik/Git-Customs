875da90affbe2263f663e6cb7d795c2675548a45
Dmytro Avilov <dmytro.avilov@flexbricks.com>
2023-09-21 18:59:50 +0300

    create pull request by commit

--- create_pull_request.sh
+++ create_pull_request.sh
@@ -1,13 +1,44 @@
 # #!/bin/bash
 
+editor=$(git config --get core.editor)
+gitDir=$(git rev-parse --git-dir)
+pullRequestMessageFile=$gitDir/PULLREQUEST_MSG
+pullRequestSelectBranchFile=$gitDir/PULLREQUEST_BRANCHES
+
 repositoryName=$(basename $(git remote get-url origin))
 
 [[ "$repositoryName" == "origin" ]] \
     && echo "This repository doesn't have remote repository" \
     && exit 1
 
-sourceBranch=$(git branch --show-current)
 destinationBranch="${1:-develop}"
+
+if [[ -z $2 ]]
+then 
+    sourceBranch=$(git branch --show-current)
+else
+    sourceBranch=($(git branch --contains $2 --format='%(refname:short)'))
+fi
+
+if [[ ${#sourceBranch[@]} -gt 1 ]]
+then
+    echo ${sourceBranch[0]} > $pullRequestSelectBranchFile
+    for branch in ${sourceBranch[@]:1} 
+    do
+        echo $branch >> $pullRequestSelectBranchFile
+    done
+    echo \#Multiple branches for this commit are detected. >> $pullRequestSelectBranchFile
+    echo \#Please move the desired branch to the first line. >> $pullRequestSelectBranchFile
+    echo \#Lines starting with '#' will be ignored, and an empty file aborts the pull request creation. >> $pullRequestSelectBranchFile
+
+    $editor $pullRequestSelectBranchFile
+
+    while IFS= read -r line || [ -n "$line" ]
+    do
+        [[ $line =~ ^#.* ]] && continue
+        sourceBranch=$line
+    done < $pullRequestSelectBranchFile
+fi
 
 pullRequestTitle=$(git log -1 --pretty=%B | head -1)
 pullRequestDescription=
@@ -21,16 +52,13 @@ do
 done <<< "$(git log $sourceBranch --not origin/$destinationBranch --pretty=%B)"
 pullRequestDescription=${pullRequestDescription%$'\n'}
 
-gitDir=$(git rev-parse --git-dir)
-pullRequestMessageFile=$gitDir/PULLREQUEST_MSG
-
 echo $pullRequestTitle > $pullRequestMessageFile
-    echo "$pullRequestDescription" >> $pullRequestMessageFile
+echo "$pullRequestDescription" >> $pullRequestMessageFile
 echo \#Please check the title and description for your pull request. >> $pullRequestMessageFile
 echo \#First line will be the title, other lines will be description. >> $pullRequestMessageFile
 echo \#Lines starting with '#' will be ignored, and an empty file aborts the pull request creation. >> $pullRequestMessageFile
 
-$(git config --get core.editor) $pullRequestMessageFile
+$editor $pullRequestMessageFile
 
 firstLineFetched=false
 pullRequestDescription=
@@ -43,6 +71,7 @@ do
         pullRequestTitle="$line"
         firstLineFetched=true
     else
+        [[ -z $line ]] && continue
         pullRequestDescription="$pullRequestDescription"$'\n'"$line"
     fi
 done < $pullRequestMessageFile
